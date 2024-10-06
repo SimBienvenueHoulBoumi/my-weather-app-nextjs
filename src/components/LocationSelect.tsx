28890dfca89f86f2b52990d5292b5ed4605f1537
Sim <64483242+SimBienvenueHoulBoumi@users.noreply.github.com>
2024-10-06 20:11:18 +0200

    correct time

--- src/components/LocationSelect.tsx
+++ src/components/LocationSelect.tsx
@@ -1,45 +1,58 @@
-"use client";
-
-import { LocationSelectProps } from "@/utils/locationInterface";
-import React, { useState } from "react";
-import { IoMdArrowDropleft } from "react-icons/io";
+import { useState } from "react";
+import { IoMdArrowDropdown, IoMdArrowDropleft } from "react-icons/io";
+import { Location, LocationSelectProps } from "@/utils/locationInterface";
 
 const LocationSelect: React.FC<LocationSelectProps> = ({
   locations,
   selectedCity,
   onChange,
 }) => {
+  const [isOpen, setIsOpen] = useState(false);
   const [isHovered, setIsHovered] = useState(false);
 
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleSelect = (city: string) => {
+    onChange(city);
+    setIsOpen(false);
+  };
+
   return (
-    <div className="mb-4 w-full">
-      <label
-        htmlFor="location-select"
-        className="block text-sm font-medium leading-6 text-gray-900"
+    <div className="relative w-full">
+      {/* Bouton pour déclencher le dropdown */}
+      <button
+        className="flex items-center justify-between h-10 px-3 w-full border border-gray-400 focus:outline-none bg-white rounded-md cursor-pointer"
+        onClick={toggleDropdown}
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
       >
-        Choose a location
-      </label>
-      <div className="relative mt-2">
-        <select
-          id="location-select"
-          value={selectedCity}
-          onChange={(e) => onChange(e.target.value)}
-          onMouseEnter={() => setIsHovered(true)} // Activer sur survol
-          onMouseLeave={() => setIsHovered(false)} // Désactiver sur survol
-          className="relative w-full cursor-pointer rounded-md bg-white py-2 px-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 sm:text-sm sm:leading-6"
-        >
-          {locations.map((location, idx) => (
-            <option key={idx} value={location.name}>
-              {location.name}
-            </option>
-          ))}
-        </select>
-        <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
-          {isHovered ? (
-            <IoMdArrowDropleft className="h-5 w-5 text-gray-400" />
-          ) : null}
-        </span>
-      </div>
+        <span className="text-sm leading-none">{selectedCity}</span>
+        {/* Changement d'icône en fonction du hover */}
+        {isHovered ? (
+          <IoMdArrowDropleft className="h-5 w-5 text-gray-600" />
+        ) : (
+          <IoMdArrowDropdown className="h-5 w-5 text-gray-600" />
+        )}
+      </button>
+
+      {/* Menu Dropdown */}
+      {isOpen && (
+        <div className="absolute z-10 w-full mt-1 border border-gray-400 bg-white shadow-lg rounded-md">
+          <div className="max-h-40 overflow-y-auto"> {/* Ajout de max-height et overflow */}
+            {locations.map((location: Location, idx) => (
+              <a
+                key={idx}
+                className="flex items-center h-8 px-3 text-sm hover:bg-blue-300 cursor-pointer"
+                onClick={() => handleSelect(location.name)}
+              >
+                {location.name}
+              </a>
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
--- src/components/WeatherDisplay.tsx
+++ src/components/WeatherDisplay.tsx
@@ -11,11 +11,29 @@ const WeatherDisplay: React.FC<WeatherDisplayProps> = ({
     return null;
   }
 
+  // Fonction pour formater la date et l'heure
+  const formatDateTime = (dateTime: string) => {
+    const dateObj = new Date(dateTime);
+    const optionsDate: Intl.DateTimeFormatOptions = { year: 'numeric', month: 'long', day: 'numeric' };
+    const optionsTime: Intl.DateTimeFormatOptions = { hour: '2-digit', minute: '2-digit' };
+    
+    const formattedDate = dateObj.toLocaleDateString(undefined, optionsDate);
+    const formattedTime = dateObj.toLocaleTimeString(undefined, optionsTime);
+
+    return { formattedDate, formattedTime };
+  };
+
+  // Récupérer la date et l'heure formatées
+  const { formattedDate, formattedTime } = formatDateTime(weatherData.time);
+
   return (
     <div className="bg-blue-100 m-2 p-4 rounded-sm w-full text-center shadow-md">
       <h2 className="text-lg font-semibold">Weather for {selectedCity}</h2>
       <p className="mt-2">
-        <strong>Time:</strong> {weatherData.time}
+        <strong>Date:</strong> {formattedDate}
+      </p>
+      <p>
+        <strong>Time:</strong> {formattedTime}
       </p>
       <p>
         <strong>Temperature:</strong> {weatherData.temperature}°C
